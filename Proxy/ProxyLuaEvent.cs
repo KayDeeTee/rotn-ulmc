bc51d4d051c932b1e7bc02886017809359442fab
Lucas LaBuff <lucasbeef00@gmail.com>
2025-10-29 10:36:58 +0100

    expose event parameters for custom events

--- Proxy/ProxyLuaEvent.cs
+++ Proxy/ProxyLuaEvent.cs
@@ -1,4 +1,5 @@
 using MoonSharp.Interpreter;
+using UnityEngine;
 
 namespace UIPlugin;
 
@@ -11,4 +12,10 @@ class ProxyLuaEvent
         target = t;
     }
     public string Type => target.CustomType;
+
+    public string GetString(string key) => target.GetString(key);
+    public bool? GetBool(string key) => target.GetBool(key);
+    public int? GetInt(string key) => target.GetInt(key);
+    public float? GetFloat(string key) => target.GetFloat(key);
+    public Color? GetColor(string key) => target.GetColor(key);
 }
